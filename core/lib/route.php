e4b8cc68f55912534f9e0e5de0e25e89c2ae876a
dyedd <1176996982@qq.com>
2020-04-08 20:28:18 +0800

    优化get方法

--- core/lib/route.php
+++ core/lib/route.php
@@ -17,6 +17,9 @@ class route
         if (isset($_SERVER['QUERY_STRING']) && $_SERVER['QUERY_STRING'] != ""){
             $path = $_SERVER['QUERY_STRING'];
             $rePath = trim($path,"s=");
+            // 匹配index/index 去除？n = 1
+            if (preg_match('/(\?|&.*)/', $rePath))
+                $rePath = preg_replace('/(\?|&.*)/', '', $rePath);
             $pathArr = explode("/",$rePath);
             if (!empty($pathArr[0]))
                 $this->ctrl = $pathArr[0];
@@ -24,21 +27,6 @@ class route
                $this->action = $pathArr[1];
             else
                $this->action = conf::get('ACTION', 'route');
-            // url 多余部分转换成 GET
-            // index/index/id/q
-            unset($pathArr[0]);
-            unset($pathArr[1]);
-            // 重置索引
-            $pathArr = &array_values($pathArr);
-            $count = count($pathArr);
-            if ($count > 2)
-                foreach ($pathArr as $key => $value)
-                    if ($key % 2 == 0){
-                        // 提供get与post
-                        $_GET[$pathArr[$key]] = $pathArr[$key + 1];
-
-                        $_POST[$pathArr[$key]] = $pathArr[$key + 1];
-                    }
         }else{
             $this->ctrl = conf::get('CTRL', 'route');
             $this->action = conf::get('ACTION', 'route');
